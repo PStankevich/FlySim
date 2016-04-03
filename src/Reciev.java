48dd803a8ede85cf61d0702ebc1e3195e7f290b4
sliver449 <sliver449@gmail.com>
2016-04-04 00:01:58 +0300

    Рабочий. Шлет размер массива, данные и символ конца строки. Не тормозит. Данные отсылает полностью :)

--- src/Reciev.java
+++ src/Reciev.java
@@ -2,6 +2,8 @@
  * Created by sliver_note on 16.12.2015.
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.invoke.SerializedLambda;
 import jssc.SerialPort;
 import jssc.SerialPortEvent;
@@ -17,6 +19,7 @@ public class Reciev {
 
 
     private static SerialPort serialPort;
+    private static String msg;
     private static int count;
     private static int countNum;
 
@@ -27,9 +30,10 @@ public class Reciev {
             int port = 20083;
 
 
+
             serialPort = new SerialPort("COM4");
             serialPort.openPort();
-            serialPort.setParams(SerialPort.BAUDRATE_115200,
+            serialPort.setParams(SerialPort.BAUDRATE_57600,
                     SerialPort.DATABITS_8,
                     SerialPort.STOPBITS_1,
                     SerialPort.PARITY_NONE);
@@ -37,32 +41,22 @@ public class Reciev {
             DatagramSocket dsocket = new DatagramSocket(port);
             byte[] buffer = new byte[2048];
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//            serialPort.writeString(String.valueOf(reader.readLine()));
             while(true){
             dsocket.receive(packet);
-            String msg = new String(buffer, 0, packet.getLength());
+              msg = new String(buffer, 0, packet.getLength());
 //                System.out.println(packet.getAddress().getHostName() + ": " + buffer.length + " === "
 //                        + msg);
-            int[] numArr = Arrays.stream(msg.split(" ")).mapToInt(Integer::parseInt).toArray();
-            System.out.print(numArr.length + " :");
-            for (int i = 0; i < numArr.length; i++) {
-                System.out.print(numArr[i] + ", ");
-            }
 
-                System.out.println(buffer.length);
-                System.out.println();
+            int[] numArr = Arrays.stream(msg.split(" ")).mapToInt(Integer::parseInt).toArray();
+//                transmit(numArr);
+                System.out.println(numArr.length + " " + msg + '/');
+                serialPort.writeString(numArr.length + msg + '/');
+//                reciev();
             packet.setLength(buffer.length);
-                    serialPort.writeString("s");
-                    serialPort.writeString(Integer.toString(numArr.length));
-                    serialPort.writeString(":");
-                    for (int i = 0; i < numArr.length/2; i++) {
-                        serialPort.writeString(Integer.toString(numArr[i]));
-                        serialPort.writeString("/");
-                }
-                serialPort.writeString("d");
-                for (int i = numArr.length/2 + 1; i < numArr.length; i++) {
-                    serialPort.writeString(Integer.toString(numArr[i]));
-                    serialPort.writeString("/");
-                }
+
 
         }
         } catch (Exception e) {
@@ -73,6 +67,37 @@ public class Reciev {
         }
     }
 
+//    public static void transmit(int[] numArr){
+////        System.out.println("<Start>");
+////        System.out.print("<" + numArr.length + ">");
+////        for (int i = 0; i < numArr.length; i++) {
+////            System.out.print(numArr[i] + ", ");
+////        }
+//        System.out.println();
+//        try {
+////            while(!serialPort.readString().equals("Ready!"));
+//            serialPort.writeString(msg);
+////            serialPort.writeString("<" + Integer.toString(numArr.length) + ">");
+////            for (int i = 0; i < numArr.length; i++) {
+////                serialPort.writeString(Integer.toString(numArr[i]));
+////                serialPort.writeString("/");
+////            }
+////            serialPort.writeString("<Stop>");
+//        } catch (SerialPortException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+//    public static void reciev(){
+//        try {
+//            while (!serialPort.readString().equals("Ready"));
+//        } catch (SerialPortException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
 
     private static class PortReader implements SerialPortEventListener {
 
@@ -80,6 +105,7 @@ public class Reciev {
             if(event.isRXCHAR() && event.getEventValue() > 0){
                 try {
                     String data = serialPort.readString(event.getEventValue());
+                    System.out.println();
                     System.out.println(data);
                 }
                 catch (SerialPortException ex) {
@@ -87,10 +113,5 @@ public class Reciev {
                 }
             }
         }
-    }
-    public static int timer() throws InterruptedException {
-        Thread.sleep(10);
-        count++;
-        return count;
     }
 }
